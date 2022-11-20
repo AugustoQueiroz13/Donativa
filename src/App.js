8f2fb068b1e388a593eba509b78675c92de4bcab
Augusto Queiroz <96686983+AugustoQueiroz13@users.noreply.github.com>
2022-11-20 10:46:24 -0300

    Donativa
    Upload dos arquivos finais do sistema Donativa desenvolvido pelo Squad 3 do curso Recode Pro Ciclo 2022

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Modal from './Components/Modal';
+
+import Rotas from './Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <Rotas />
+    </div>
+  );
+}
+
+export default App;
